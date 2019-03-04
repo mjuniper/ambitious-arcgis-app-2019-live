b371fdc0d6cf1dd3614d8393c8467f274e8af60c
Tom Wayson <tom@tomwayson.com>
2019-03-04 15:45:50 -0800

    show items on map

--- app/utils/map.js
+++ app/utils/map.js
@@ -1,17 +1,22 @@
 import esriLoader from 'esri-loader';
 
+// NOTE: module, not global scope
+let _Graphic;
+
 // lazy load the ArcGIS API modules and CSS
 // then create a new map view at an element
 export function loadMap(element, mapOptions) {
   return esriLoader.loadModules(
-    ['esri/Map', 'esri/views/MapView'],
+    ['esri/Map', 'esri/views/MapView', 'esri/Graphic'],
     // NOTE: keep this current w/ the latest version of the JSAPI
     { css: 'https://js.arcgis.com/4.10/esri/css/main.css' }
-  ).then(([Map, MapView]) => {
+  ).then(([Map, MapView, Graphic]) => {
     if (!element) {
       // component or app was likely destroyed
       return;
     }
+    // hold onto the graphic class for later use
+    _Graphic = Graphic;
     // Create the Map
     const map = new Map(mapOptions);
     // show the map at the element
@@ -30,4 +35,44 @@ export function loadMap(element, mapOptions) {
       return view;
     });
   });
+}
+
+export function showItemsOnMap(view, items, symbol, popupTemplate) {
+  if (!_Graphic) {
+    throw new Error('You must load a map before creating new graphics');
+  }
+  if (!view || !view.ready) {
+    return;
+  }
+  // clear any existing graphics (if any)
+  view.graphics.removeAll();
+  if (!items) {
+    return;
+  }
+  // convert items to graphics and add to the view
+  items.forEach(item => {
+    const graphicJson = itemToGraphicJson(item, symbol, popupTemplate);
+    view.graphics.add(new _Graphic(graphicJson));
+  });
+}
+
+export function itemToGraphicJson(item, symbol, popupTemplate) {
+  const geometry = coordsToExtent(item.extent);
+  return { geometry, symbol, attributes: item, popupTemplate };
+}
+
+// expect [[-53.2316, -79.8433], [180, 79.8433]] or []
+function coordsToExtent(coords) {
+  if (coords && coords.length === 2) {
+    return {
+      type: 'extent',
+      xmin: coords[0][0],
+      ymin: coords[0][1],
+      xmax: coords[1][0],
+      ymax: coords[1][1],
+      spatialReference: {
+        wkid:4326
+      }
+    };
+  }
 }
--- tests/unit/utils/map-test.js
+++ tests/unit/utils/map-test.js
@@ -1,4 +1,4 @@
-import { loadMap } from '../../../utils/map';
+import { loadMap, itemToGraphicJson } from '../../../utils/map';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | map', function(/* hooks */) {
@@ -8,4 +8,34 @@ module('Unit | Utility | map', function(/* hooks */) {
     let result = typeof loadMap === 'function';
     assert.ok(result);
   });
+});
+
+test('itemToGraphicJson', function(assert) {
+  const item = {
+    extent: [[-53.2316, -79.8433], [180, 79.8433]],
+    title: 'Test Item',
+    snippet: 'this is a test item'
+  };
+  let result = itemToGraphicJson(item);
+  assert.deepEqual(result.geometry, {
+    type: 'extent',
+    xmin: -53.2316,
+    ymin: -79.8433,
+    xmax: 180,
+    ymax: 79.8433,
+    spatialReference:{
+      wkid:4326
+    }
+  });
+  assert.deepEqual(result.attributes, item);
+});
+
+test('itemToGraphicJson with no extent', function(assert) {
+  const item = {
+    title: 'Item with no extent',
+    snippet: 'sometimes items do not have extents'
+  };
+  let result = itemToGraphicJson(item);
+  assert.equal(result.geometry, null);
+  assert.deepEqual(result.attributes, item);
 });
